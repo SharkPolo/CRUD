02c3c533e218fdb7a1c78f44c15dea7f77483f41
Vinicius <23589928+SharkPolo@users.noreply.github.com>
2024-01-17 18:46:23 -0300

    Update add.php

--- add.php
+++ add.php
@@ -1,34 +1,35 @@
 <html>
 <head>
-	<title>Add Data</title>
+	<title>Cadastro</title>
 </head>
 
 <body>
-	<h2>Add Data</h2>
-	<p>
-		<a href="index.php">Home</a>
-	</p>
+	<h2>Cadastro</h2>
+	
 
 	<form action="addAction.php" method="post" name="add">
 		<table width="25%" border="0">
 			<tr> 
-				<td>Name</td>
+				<td>Nome</td>
 				<td><input type="text" name="name"></td>
 			</tr>
 			<tr> 
-				<td>Age</td>
+				<td>Idade:</td>
 				<td><input type="text" name="age"></td>
 			</tr>
 			<tr> 
-				<td>Email</td>
+				<td>E-mail</td>
 				<td><input type="text" name="email"></td>
 			</tr>
 			<tr> 
 				<td></td>
-				<td><input type="submit" name="submit" value="Add"></td>
+				<td><input type="submit" name="submit" value="Cadastrar"></td>
 			</tr>
 		</table>
 	</form>
+	<p>
+		<a href="index.php">Voltar</a>
+	</p>
 </body>
 </html>
 
